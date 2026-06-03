aff4e816617f33f0cf7faeec48948142fbe0bab1
Florentin Bouchez <44985686+iTEEECH@users.noreply.github.com>
2026-06-02 21:37:19 -0400

    fix(reactive_ble_mobile): Replace mutableMapOf to ConcurrentHashMap

--- packages/reactive_ble_mobile/android/src/main/kotlin/com/signify/hue/flutterreactiveble/ble/ReactiveBleClient.kt
+++ packages/reactive_ble_mobile/android/src/main/kotlin/com/signify/hue/flutterreactiveble/ble/ReactiveBleClient.kt
@@ -30,10 +30,10 @@ import io.reactivex.Single
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.subjects.BehaviorSubject
 import java.util.UUID
+import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.TimeUnit
 import kotlin.collections.component1
 import kotlin.collections.component2
-
 @Suppress("TooManyFunctions")
 open class ReactiveBleClient(private val context: Context) : BleClient {
     private val connectionQueue = ConnectionQueue()
@@ -47,14 +47,14 @@ open class ReactiveBleClient(private val context: Context) : BleClient {
 
         lateinit var rxBleClient: RxBleClient
             internal set
-        internal var activeConnections = mutableMapOf<String, DeviceConnector>()
+        internal var activeConnections: MutableMap<String, DeviceConnector> = ConcurrentHashMap()
     }
 
     override val connectionUpdateSubject: BehaviorSubject<ConnectionUpdate>
         get() = connectionUpdateBehaviorSubject
 
     override fun initializeClient() {
-        activeConnections = mutableMapOf()
+        activeConnections = ConcurrentHashMap()
         rxBleClient = RxBleClient.create(context)
     }
 
